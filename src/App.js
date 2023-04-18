a170d149f20907e7e1d4b4ec5fae9d22b8781bdd
Fiona Dodge <fiona.dodge11@gmail.com>
2023-04-18 23:13:48 +0100

    react started

--- src/App.js
+++ src/App.js
@@ -3,21 +3,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="Column">
+      <div class="menu_layout">
+        <h1>Cocktails</h1>
+        <div class="cocktail_cards">
+          <h5>Expresso Martini £8.30</h5>
+          <p>Double expresso, ice cold with vodka. Kahlua & a dash of vanilla syrup</p>
+        </div>
+        <div class="cocktail_cards">
+          <h5>Pornstar Martini £8.60</h5>
+          <p>Absolute Vanilla vodka, Passoa passionfruit liqueur, passionfruit puree and pineapple & lemon juice. Served with a prosecco shot.</p>
+        </div>
+        <div class="cocktail_cards">
+          <h5>Sex on the Beach £7.80</h5>
+          <p>JJ Whitley vodka, peach schnapps, orange & cranberry juice.</p>
+        </div>
+        <div class="cocktail_cards">
+          <h5>Popcorn Old Fashioned £8.30</h5>
+          <p>Classic Buffalo Trace Bourbon. Old fashioned made with popcorn syrup.</p>
+        </div>
+        <div class="cocktail_photo">
+          <img src=""></img>
+          <h2>"A sweet fruity taste knocked back with some bubbles - a must drink"</h2>
+          <p>Isabel</p>
+          <p>Pornstar Martini</p>
+        </div>
+      </div>
     </div>
   );
 }
